927e96aa5763275c285882f188c6a8c9a2ed2abb
Brian-Lookabaugh <brianlookabaugh21@gmail.com>
2022-11-27 13:01:26 -0600

    Outline of data collection and results

--- Analysis/PKO-Analysis.R
+++ Analysis/PKO-Analysis.R
@@ -2,8 +2,32 @@
 
 ### Data Collection and Cleaning
 
+## UCDP Data
+
+## PKO Data
+
+## V-Dem Data
+
+## (Dependent Variable Data)
+
 ### Generate a Map of PKO Distribution
 
 ### Synthetic Control Set-Up
 
 ### Synthetic Control Graphics and Tables
+
+## Time Series Plot
+
+## Difference in Synthetic and Observed Plot
+
+## Unit and Variable Weights Plot
+
+## Table Comparing Synthetic to Observed
+
+## Unit Placebos
+
+## Unit Placebos With Extreme Values
+
+## Post/Pre-MSPE
+
+## Tables With Significance of Unit Treatments
