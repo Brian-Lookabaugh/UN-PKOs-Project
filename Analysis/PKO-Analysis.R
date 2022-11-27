8e4b8bcfd8410f8265500f809d99f7ef6bd717d8
Brian-Lookabaugh <brianlookabaugh21@gmail.com>
2022-11-27 16:29:41 -0600

    Data collection

--- Analysis/PKO-Analysis.R
+++ Analysis/PKO-Analysis.R
@@ -5,6 +5,7 @@
 pacman::p_load(
   "tidyverse", # Data Manipulation and Visualization
   "tidysynth", # Tidy Implementation of the Synthetic Control Method
+  "DescTools", # Carrying Values of Observations Forward
   "sf", # Maps
   "rnaturalearth", # Maps
   "rnaturalearthdata", # Maps
@@ -23,15 +24,37 @@ ucdp <- ucdp_prio_acd_221
 # Drop Non-Civil War Cases (1K Threshold)
 
 ucdp <- ucdp %>%
-  filter(type_of_conflict == 3)
-
-# Collapse Data Into Country-Year Units
+  mutate(gwno_a = as.numeric(gwno_a)) %>% # Convert the country code to a numeric
+  filter(type_of_conflict == 3 & cumulative_intensity == 1) %>%
+  mutate(civ_war = 1) %>%
+  # Drop European Outlier
+  # Drop Coups (Thyne 2017)
+  
+  # Collapse Data Into Country-Year Units
+  group_by(gwno_a, year) %>%
+  summarise(civ_war = max(civ_war)) %>%
+  ungroup()
 
 # Merge COW Country-Year Data
 
-# Generate Civil War Dummy Variable
+states <- read.csv("C:/Users/brian/Desktop/Peacebuilding Dissertation/PKO/Data/system2016.csv")
 
-# Drop Non-Conflict Cases
+ucdp <- full_join(ucdp, states,
+                   by = c("gwno_a" = "ccode", "year"))
+
+# Drop Cases That Have Never Experienced a Civil War
+
+ucdp <- ucdp %>%
+  rename(ccode = gwno_a) %>%
+  arrange(ccode, year) %>%
+  group_by(ccode) %>%
+  mutate(prior_civ_war = LOCF(civ_war)) %>%
+  ungroup() %>%
+  filter(prior_civ_war == 1) %>%
+  mutate(civ_war = if_else( # Replace NA Civil War Values With 0
+    is.na(civ_war), 0, civ_war
+  )) %>%
+  select(-c(version, prior_civ_war))
 
 # Drop Cases Where Conflict Never Ends
 
@@ -40,6 +63,8 @@ ucdp <- ucdp %>%
 # Generate Conflict Termination Variable
 
 # Merge Relevant UCDP Information (War Outcome, Conflict Intensity)
+
+# Clean Up Data
 
 ## PKO Data
 
