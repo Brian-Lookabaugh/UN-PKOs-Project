0463153a77af8eac8d96aea0cf52f573c16cadd3
Brian-Lookabaugh <brianlookabaugh21@gmail.com>
2022-12-13 14:54:40 -0600

    Minor updates for analysis outline

--- Analysis/DID-Analysis.R
+++ Analysis/DID-Analysis.R
@@ -14,7 +14,9 @@ pacman::p_load(
 ###############---------Data Collection and Cleaning---------###############
 ############################################################################
 
-# Load Geo-Coded PKO Data
+# Load and Clean UCDP Georeferenced Event Data (GED)
+
+# Load and Clean UCDP Managing Intrastate Conflict (MIC) Africa Data
 
 ############################################################################
 ###############------------Map of PKO Distribution-----------###############
@@ -45,11 +47,13 @@ pacman::p_load()
 
 # Base Model (M1): Two Time Periods, Binary Treatment, No Controls
 
-# M2: Base Model With Controls
+# M2: Base Model With IPW
 
-# M3: Base Model With Multiple Time Periods
+# M3: Base Model With Controls
 
-# M4: Base Model With Continuous Treatment
+# M4: Base Model With Multiple Time Periods
+
+# M5: Base Model With Continuous Treatment
 
 ## Sensitivity Analysis
 
