b6aa56a41aacad3855cb0868abf80218bb85cf0a
Brian-Lookabaugh <brianlookabaugh21@gmail.com>
2022-12-23 10:56:02 -0600

    Outline IPW project

--- Analysis/IPW-Analysis.R
+++ Analysis/IPW-Analysis.R
@@ -2,23 +2,160 @@
 ###############---------------PKO IPW Analysis---------------###############
 ############################################################################
 
+############################################################################
+###############---------Data Collection and Cleaning---------###############
+############################################################################
+
 pacman::p_load(
   "tidyverse", # Data Manipulation and Visualization
   install = FALSE
 )
 
 ############################################################################
-###############---------Data Collection and Cleaning---------###############
+###############----------------------DAG---------------------###############
 ############################################################################
 
-############################################################################
-###############------------Map of PKO Distribution-----------###############
-############################################################################
+pacman::p_load(
+  "ggdag", # Plot DAGs with ggplot
+  "dagitty" # DAG Math
+)
 
 ############################################################################
-###############-----------------IPW Analysis-----------------###############
+###############--------------IPW/Matching Set-Up-------------###############
 ############################################################################
+
+### Aggregate Deaths
+# IPW: Dummy PKO Treatment, ATE
+
+# IPW: Continuous PKO Treatment, ATE
+
+# Inspect for Extreme Weights
+
+# Matching: Mahalanobis Distance, Dummy PKO Treatment, ATE
+
+# Matching: CEM, Dummy PKO Treatment, ATE
+
+# Graph Weights
+
+# Balancing Tables
+
+# Density Plots
+
+### State-Based Deaths
+# IPW: Dummy PKO Treatment, ATE
+
+# IPW: Continuous PKO Treatment, ATE
+
+# Inspect for Extreme Weights
+
+# Matching: Mahalanobis Distance, Dummy PKO Treatment, ATE
+
+# Matching: CEM, Dummy PKO Treatment, ATE
+
+# Graph Weights
+
+# Balancing Tables
+
+# Density Plots
+
+### Non-State Based Deaths
+# IPW: Dummy PKO Treatment, ATE
+
+# IPW: Continuous PKO Treatment, ATE
+
+# Inspect for Extreme Weights
+
+# Matching: Mahalanobis Distance, Dummy PKO Treatment, ATE
+
+# Matching: CEM, Dummy PKO Treatment, ATE
+
+# Graph Weights
+
+# Balancing Tables
+
+# Density Plots
+
+### One-Sided Violence
+# IPW: Dummy PKO Treatment, ATE
+
+# IPW: Continuous PKO Treatment, ATE
+
+# Inspect for Extreme Weights
+
+# Matching: Mahalanobis Distance, Dummy PKO Treatment, ATE
+
+# Matching: CEM, Dummy PKO Treatment, ATE
+
+# Graph Weights
+
+# Balancing Tables
+
+# Density Plots
+
+############################################################################
+###############-------------IPW/Matching Analysis------------###############
+############################################################################
+
+### Aggregate Deaths
+# IPW: Dummy PKO Treatment, ATE
+
+# IPW: Continuous PKO Treatment, ATE
+
+# Matching: Mahalanobis Distance, Dummy PKO Treatment, ATE
+
+# Matching: CEM, Dummy PKO Treatment, ATE
+
+# Covariate Adjustment
+
+# Plot Effects
+
+### State-Based Deaths
+# IPW: Dummy PKO Treatment, ATE
+
+# IPW: Continuous PKO Treatment, ATE
+
+# Matching: Mahalanobis Distance, Dummy PKO Treatment, ATE
+
+# Matching: CEM, Dummy PKO Treatment, ATE
+
+# Covariate Adjustment
+
+# Plot Effects
+
+### Non-State Based Deaths
+# IPW: Dummy PKO Treatment, ATE
+
+# IPW: Continuous PKO Treatment, ATE
+
+# Matching: Mahalanobis Distance, Dummy PKO Treatment, ATE
+
+# Matching: CEM, Dummy PKO Treatment, ATE
+
+# Covariate Adjustment
+
+# Plot Effects
+
+### One-Sided Violence
+# IPW: Dummy PKO Treatment, ATE
+
+# IPW: Continuous PKO Treatment, ATE
+
+# Matching: Mahalanobis Distance, Dummy PKO Treatment, ATE
+
+# Matching: CEM, Dummy PKO Treatment, ATE
+
+# Covariate Adjustment
+
+# Plot Effects
 
 ############################################################################
 ###############-------------Sensitivity Analysis-------------###############
 ############################################################################
+
+### Aggregate Deaths
+
+### State-Based Deaths
+
+### Non-State Based Deaths
+
+### One-Sided Violence
