41b8cff1b97892a5dd0df916ec8470d81f3f7172
Brian-Lookabaugh <brianlookabaugh21@gmail.com>
2022-12-24 12:14:29 -0600

    Created DAG

--- Analysis/IPW-Analysis.R
+++ Analysis/IPW-Analysis.R
@@ -14,30 +14,24 @@ pacman::p_load(
   install = FALSE
 )
 
-# Custom ggplot Function
-
 # Generate the DAG
 
-pko_dag <- dagify(peace ~ pko + democ + nat_res + peace_agg + gov_cap + pop + eth_frac +
-                    dev + bat_deaths,
-                  pko ~ bat_deaths + dev + gov_cap + nat_res,
+pko_dag <- dagify(peace ~ pko + democ + nat_res + gov_cap + pop + eth_frac + dev,
+                  pko ~ dev + gov_cap + nat_res,
                   democ ~ nat_res,
-                  peace_agg ~ gov_cap + bat_deaths + pko + nat_res,
                   gov_cap ~ dev + nat_res,
                   pop ~ dev,
                   dev ~ eth_frac + nat_res + democ,
-                  bat_deaths ~ eth_frac + gov_cap + peace_agg,
                   exposure = "pko",
-                  outome = "peace",
-                  coords = list(x = c(peace = 7, pko = 3, democ = 4, nat_res = 5, peace_agg = 6,
-                                      gov_cap = 7, pop = 1, eth_frac = 4, dev = 5, bat_deaths = 6),
-                                y = c(peace = 2, pko = 2, democ = 3, nat_res = 1, peace_agg = 3,
-                                      gov_cap = 4, pop = 5, eth_frac = 5, dev = 2, bat_deaths = 2)),
+                  outcome = "peace",
+                  coords = list(x = c(peace = 3.5, pko = 1, democ = 8, nat_res = 1,
+                                      gov_cap = 3, pop = 8, eth_frac = 8, dev = 4),
+                                y = c(peace = 3, pko = 4, democ = 3, nat_res = 1,
+                                      gov_cap = 4, pop = 1, eth_frac = 4, dev = 1)),
                   labels = c(peace = "Peace", pko = "PKO", democ = "Democracy",
-                             nat_res = "Natural Resources", peace_agg = "Peace Agreement",
-                             gov_cap = "Government Capacity", pop = "Population",
-                             eth_frac = "Ethnic Diversity", dev = "Development",
-                             bat_deaths = "Battle Deaths"))
+                             nat_res = "Natural Resources", gov_cap = "Government Capacity", 
+                             pop = "Population", eth_frac = "Ethnic Diversity", 
+                             dev = "Development"))
 
 # Convert DAG Object Into a Tidy Frame for Plotting
 
@@ -45,19 +39,26 @@ tidy_pko_dag <- pko_dag %>%
   tidy_dagitty() %>%
   node_status()
 
-status_color <- c(exposure = "#21918c", outcome = "#440154", latent = "grey50")
+status_colors <- c(exposure = "#21918c", outcome = "#440154", latent = "grey50")
 
 # Further Graph Customization
 
-ggplot(tidy_pko_dag, aes(x = x, y = y, xend = xend, yend = yend)) +
+dag <- ggplot(tidy_pko_dag, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_dag_edges() +
-  geom_dag_point(aes(color = "status")) +
-  geom_dag_label_repel(aes(label = label, fill = status), seed = 1234,
+  geom_dag_point(aes(color = status)) +
+  geom_dag_label_repel(aes(label = label, fill = status),
                        color = "white", fontface = "bold") +
   scale_color_manual(values = status_colors, na.value = "grey20") +
-  scale_fill_manual(values = status_color, na.value = "grey20") +
-  guides(color = FALSE, fill = FALSE) +
+  scale_fill_manual(values = status_colors, na.value = "grey20") +
+  guides(color = "none", fill = "none") +
   theme_dag()
+
+ggsave(
+  "pko_dag.png",
+  width = 6,
+  height = 4,
+  path = "C:/Users/brian/Desktop/Peacebuilding Dissertation/PKO/Graphics"
+)
 
 ############################################################################
 ###############---------Data Collection and Cleaning---------###############
