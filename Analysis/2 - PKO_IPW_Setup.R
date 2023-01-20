31591f9c2d6ed1004347bd02ae3edd055b41a0d1
Brian-Lookabaugh <brianlookabaugh21@gmail.com>
2023-01-19 22:28:08 -0600

    More balancing work

--- Analysis/2 - PKO_IPW_Setup.R
+++ Analysis/2 - PKO_IPW_Setup.R
@@ -40,17 +40,17 @@ ggsave(
   path = "C:/Users/brian/Desktop/Peacebuilding Dissertation/UN PKOs Project/Graphics"
 )
 
-# Create Mahalanobis Nearest-Neighbor (NN) Matched Set: 4 Lags, 1 Match
+# Create Mahalanobis Nearest-Neighbor (NN) Matched Set: 4 Lags, Up to 5 Matches
 merged <- merged %>%
   mutate(ccode = as.integer(ccode)) %>%
   select(-c(stateabb)) # PanelMatch Will Not Run With Non-Numeric/Integer Data
 
-nn_match_1 <- PanelMatch(lag = 4,
+nn_match_5 <- PanelMatch(lag = 4,
                        time.id = "year",
                        unit.id = "ccode",
                        treatment = "pko",
                        refinement.method = "mahalanobis",
-                       size.match = 1,
+                       size.match = 5,
                        data = merged,
                        covs.formula = ~ 
                          I(lag(lpop, 1:4)) +
@@ -63,13 +63,13 @@ nn_match_1 <- PanelMatch(lag = 4,
                        use.diagonal.variance.matrix = TRUE
                        )
 
-# Create Mahalanobis Nearest-Neighbor (NN) Matched Set: 4 Lags, Up to 3 Matches
-nn_match_3 <- PanelMatch(lag = 4,
+# Create Mahalanobis Nearest-Neighbor (NN) Matched Set: 4 Lags, Up to 10 Matches
+nn_match_10 <- PanelMatch(lag = 4,
                        time.id = "year",
                        unit.id = "ccode",
                        treatment = "pko",
                        refinement.method = "mahalanobis",
-                       size.match = 3,
+                       size.match = 10,
                        data = merged,
                        covs.formula = ~ 
                          I(lag(lpop, 1:4)) +
@@ -82,7 +82,7 @@ nn_match_3 <- PanelMatch(lag = 4,
                        use.diagonal.variance.matrix = TRUE)
 
 # Create IPW Weighted Set: 4 Lags
-ipw_match <- PanelMatch(lag = 4,
+ipw <- PanelMatch(lag = 4,
                         time.id = "year",
                         unit.id = "ccode",
                         treatment = "pko",
@@ -98,13 +98,29 @@ ipw_match <- PanelMatch(lag = 4,
                         lead = 0:4,
                         restrict.control.period = 4)
 
-# NN: Frequency Distribution of Matched Units
-
-# IPW: Frequency Distribution of Weighted Units
-
 # NN: Covariate Balance Plot Post-Refinement
+# Create a Matched Set Without Refinement
+no_ref <- PanelMatch(lag = 4,
+                     time.id = "year",
+                     unit.id = "ccode",
+                     treatment = "pko",
+                     refinement.method = "none",
+                     data = merged,
+                     covs.formula = ~ 
+                       I(lag(lpop, 1:4)) +
+                       I(lag(lmilper, 1:4)) +
+                       I(lag(ldeaths, 1:4)) +
+                       I(lag(wardur, 1:4)),
+                     size.match = 5,
+                     qoi = "att",
+                     outcome.var = "lgdppc",
+                     lead = 0:4)
+
+
 
 # IPW: Covariate Balance Plot Post-Refinement
+
+# Combined Covariate Balance Plot
 
 # NN: Covariate Balance With Lagged Outcome
 
