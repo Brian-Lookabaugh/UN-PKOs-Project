72a74ffd862e3a4269f58e6403ce2e784f613e29
Brian-Lookabaugh <brianlookabaugh21@gmail.com>
2023-01-25 10:05:57 -0600

    Graphics updates

--- Analysis/4 - PKO_Sensitivity_Analysis.R
+++ Analysis/4 - PKO_Sensitivity_Analysis.R
@@ -10,19 +10,21 @@ pacman::p_load(
 
 # Create Base Model With No Matching
 merged <- merged %>% # Renaming for Better Label on the Plot
-  rename(Democracy = democracy)
+  rename(MPpc = lmilper)
 
-m1 <- lm(lgdppc ~ pko + lmilper + lpop + Deaths + wardur + Democracy, data = merged)
+m1 <- lm(lgdppc ~ pko + MPpc + lpop + ldeaths + wardur + democracy, 
+         data = merged)
 
 # Create and Visualize the Sensitivity Estimates
-sens_results_dem <- sensemakr(m1, treatment = "pko", benchmark_covariates = "Democracy",
-                              kd = c(0.25, 0.5, 1))
+sens_results <- sensemakr(m1, treatment = "pko", 
+                          benchmark_covariates = "MPpc",
+                          kd = c(0.25, 0.5))
 
 # Renaming Labels for Better Punctuation
 x_lab <- expression(Partial ~ R^2 ~ of ~ Confounders(s) ~ With ~ the ~ Outcome)
 y_lab <- expression(Partial ~ R^2 ~ of ~ Confounders(s) ~ With ~ the ~ Treatment)
 
-plot(sens_results_dem, 
+plot(sens_results, 
      ylab = x_lab, 
      xlab = y_lab)
 
